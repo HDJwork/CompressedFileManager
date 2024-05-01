ccc335a592b88f6b780c47fea29469cdf4dfa7e3
HDJwork <hdjwork123234@gmail.com>
2024-05-02 02:37:32 +0900

    set test code

--- CFMCore/src/lib.rs
+++ CFMCore/src/lib.rs
@@ -11,38 +11,75 @@ mod SingletonManager;
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
 
     #[test]
     fn it_works() {
+        //arrange
+        use std::collections::HashSet;
+        let srcPath="../TestData/TestData.zip";
+        let testPath="../TestData/TestData1.zip";
+        let exist = match std::fs::metadata(testPath){
+            Ok(metadata)=>{metadata.is_file()}
+            Err(_) => {false}
+        };
+        if exist {
+            std::fs::remove_file(testPath).expect("std::fs::remove_file Fail!");
+        }
+        std::fs::copy(srcPath,testPath).expect("std::fs::copy Fail!");
+
         SingletonManager::startup();
-
         //let mut compressedFile=CompressedFileManager::Open("D:/Develop/CompressedFileManager/TestData/TestData.zip");
-        let path="../TestData/TestData.zip";
-        let outputPath="../TestData/TestData1.zip";
-        println!("Open : {}",path);
-        let mut compressedFile=CompressedFileManager::Open(path);
+
+        //act
+        println!("Open : {}",testPath);
+        let mut compressedFile=CompressedFileManager::Open(testPath);
         let fileList=compressedFile.GetFileList();
         println!("compressedFile.GetFileList => \r\n{}",ToSummary(&fileList));
-        
-        println!("delete file : {}",fileList[0].as_str());
-        compressedFile.DeleteFile(fileList[0].as_str());
-        compressedFile.Recompress(outputPath);
+        let mut removeList: HashSet<String> = HashSet::new();
+        removeList.insert(fileList[0].clone());
+
+        println!("delete file =>");
+        for removeFile in &removeList{
+            println!("{}",removeFile.as_str());
+            compressedFile.DeleteFile(removeFile.as_str());
+        }
+        compressedFile.Recompress(testPath);
 
         CompressedFileManager::Close(compressedFile);
 
         
-        println!("Open : {}",outputPath);
-        let mut compressedFile=CompressedFileManager::Open(outputPath);
-        let fileList=compressedFile.GetFileList();
-        println!("compressedFile.GetFileList => \r\n{}",ToSummary(&fileList));
+        println!("Open : {}",testPath);
+        let mut compressedFile=CompressedFileManager::Open(testPath);
+        let fileList_recomp=compressedFile.GetFileList();
+        println!("compressedFile.GetFileList => \r\n{}",ToSummary(&fileList_recomp));
         CompressedFileManager::Close(compressedFile);
         
-        // // let result = add(2, 2);
-        // // assert_eq!(result, 4);
+        //assert
+        let mut verificationList : Vec<String> = Vec::new();
+        for file in &fileList{
+            if removeList.contains(file) == false{
+                verificationList.push(file.clone()); 
+            }
+        }
+        let count = verificationList.len();
+        assert_eq!(verificationList.len(),fileList_recomp.len());
+        for i in 0..count{
+            assert_eq!(verificationList[i],fileList_recomp[i]);
+        }
 
+        //cleanup
         SingletonManager::cleanup();
+        
+        let exist = match std::fs::metadata(testPath){
+            Ok(metadata)=>{metadata.is_file()}
+            Err(_) => {false}
+        };
+        if exist {
+            std::fs::remove_file(testPath).expect("std::fs::remove_file Fail!");
+        }
     }
     fn ToSummary(strs:&Vec<String>)->String
     {
